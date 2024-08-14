17134b8025400fe189b39e3cb4c14709466629fc
nnyore <nnyore8@gmail.com>
2024-08-14 13:48:06 +0100

    Wed Aug 14 13:48:06 IST 2024
    Pointing Inde.php

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
     <h1>Welcome to Fabric Distribution Analysis</h1>
     <p>This platform helps you document and analyze key data for your fabric distribution business.</p>
     <div class="section-links">
-        <a href="data-collection/cost-price.php">Cost Price Analysis</a>
-        <a href="data-collection/pricing.php">Pricing Analysis</a>
-        <a href="data-collection/transportation.php">Transportation Costs</a>
-        <a href="data-collection/packaging.php">Packaging Costs</a>
+        <a href="fabric_market_analysis/data-collection/cost-price.php">Cost Price Analysis</a>
+        <a href="fabric_market_analysis/data-collection/pricing.php">Pricing Analysis</a>
+        <a href="fabric_market_analysis/data-collection/transportation.php">Transportation Costs</a>
+        <a href="fabric_market_analysis/data-collection/packaging.php">Packaging Costs</a>
     </div>
 </div>
 <?php include('includes/footer.php'); ?>
